b66b7e76a40ed49c5096675c98e59b9180b900b0
Hozefa Bharmal <hozefa.bharmal@gmail.com>
2018-10-31 21:42:18 -0700

    Initial Setup and Test

---
+++ test/test-server.js
@@ -0,0 +1,35 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+const { app, runServer, closeServer } = require("../server");
+
+
+chai.use(chaiHttp);
+
+describe("Initial Test", function(){
+    before(function(){
+        return runServer();
+    })
+
+    after(function(){
+        return closeServer();
+    });
+
+    it("should return status code 200", function(){
+        return chai
+        .request(app)
+        .get("/")
+        .then(function(res){
+            expect(res).to.have.status(200);
+        });
+    });
+
+    it("should be html", function(){
+        return chai
+        .request(app)
+        .get("/")
+        .then(function(res){
+            expect(res).to.be.html
+        })
+    })
+});
