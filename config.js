e0188f72ff92b0abac06bdb30eb58e711e28473b
Hozefa Bharmal <hozefa.bharmal@gmail.com>
2018-11-04 21:37:59 -0800

    Change local to mlab url for remote database

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/ComicPostTestDb';
+//local
+// exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/ComicPostTestDb';
+// exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/ComicPostTestDb';
+// exports.PORT = process.env.PORT || 8080;
+
+//mlab
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://test:thinkful1@ds151863.mlab.com:51863/comic-journal';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/ComicPostTestDb';
 exports.PORT = process.env.PORT || 8080;
