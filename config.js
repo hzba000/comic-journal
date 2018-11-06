a484bada730bf4ffa9678e58508905ddb712d982
Hozefa Bharmal <hozefa.bharmal@gmail.com>
2018-11-06 00:01:33 -0800

    Changed test db url, now seeded with empty json

--- config.js
+++ config.js
@@ -7,5 +7,5 @@
 
 //mlab
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://test:thinkful1@ds151863.mlab.com:51863/comic-journal';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/ComicPostTestDb';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-ComicPostTestDb';
 exports.PORT = process.env.PORT || 8080;
