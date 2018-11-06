0a3cca9107948e16e05d26212b9fcc6a8e334b83
Hozefa Bharmal <hozefa.bharmal@gmail.com>
2018-11-06 00:03:10 -0800

    included get integration test, working

--- test/test-server.js
+++ test/test-server.js
@@ -1,12 +1,108 @@
+'use strict';
+
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const expect = chai.expect;
-const { app, runServer, closeServer } = require("../server");
+const faker = require('faker');
+const mongoose = require('mongoose');
 
+const expect = chai.expect;
+const should = chai.should();
+
+const { Post } = require('../models');
+const { closeServer, runServer, app } = require('../server');
+const { TEST_DATABASE_URL } = require('../config');
 
 chai.use(chaiHttp);
 
-describe("Initial Test", function(){
+function tearDownDb() {
+  return new Promise((resolve, reject) => {
+    console.warn('Deleting database');
+    mongoose.connection.dropDatabase()
+      .then(result => resolve(result))
+      .catch(err => reject(err));
+  });
+}
+
+
+function seedPostData() {
+  console.info('seeding blog post data');
+  const seedData = [];
+  for (let i = 1; i <= 10; i++) {
+    seedData.push({
+      comicId: faker.random.number(),
+      email: faker.internet.email(),
+      content: faker.lorem.paragraph()
+    });
+  }
+  // this will return a promise
+  return Post.insertMany(seedData);
+}
+
+describe('blog posts API resource', function () {
+
+  before(function () {
+    return runServer(TEST_DATABASE_URL);
+  });
+
+  beforeEach(function () {
+    return seedPostData();
+  });
+
+  afterEach(function () {
+    return tearDownDb();
+  });
+
+  after(function () {
+    return closeServer();
+  });
+
+  describe('GET endpoint', function () {
+
+    it('should return all existing posts', function () {
+      let res;
+      return chai.request(app)
+        .get('/posts')
+        .then(_res => {
+          res = _res;
+          res.should.have.status(200);
+          res.body.should.have.lengthOf.at.least(1);
+
+          return Post.count();
+        })
+        .then(count => {
+          res.body.should.have.lengthOf(count);
+        });
+    });
+
+    it('should return posts with right fields', function () {
+      let resPost;
+      return chai.request(app)
+        .get('/posts')
+        .then(function (res) {
+
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('array');
+          res.body.should.have.lengthOf.at.least(1);
+
+          res.body.forEach(function (post) {
+            post.should.be.a('object');
+            post.should.include.keys('id', 'comicId', 'content', 'email', 'publishedAt');
+          });
+
+          resPost = res.body[0];
+          return Post.findById(resPost.id);
+        })
+        .then(post => {
+          resPost.comicId.should.equal(post.comicId);
+          resPost.email.should.equal(post.email);
+          resPost.content.should.equal(post.content);
+        });
+    });
+  });
+});
+
+describe("Initial Test for server", function(){
     before(function(){
         return runServer();
     })
@@ -17,19 +113,19 @@ describe("Initial Test", function(){
 
     it("should return status code 200", function(){
         return chai
-            .request(app)
-            .get("/")
-            .then(function(res){
-                expect(res).to.have.status(200);
+        .request(app)
+        .get("/")
+        .then(function(res){
+            expect(res).to.have.status(200);
         });
     });
 
     it("should be html", function(){
         return chai
-            .request(app)
-            .get("/")
-            .then(function(res){
-                expect(res).to.be.html
+        .request(app)
+        .get("/")
+        .then(function(res){
+            expect(res).to.be.html
         })
     })
 });
